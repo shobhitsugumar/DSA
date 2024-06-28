92c0a4b437e91529971a752564c051845fee4158
Shobith S <shobithsugumar@gmail.com>
2024-06-28 13:35:12 +0530

    insertion sort

---
+++ insertionsort.js
@@ -0,0 +1,18 @@
+let arr = [6, 5, 1, 9, 8];
+
+function insertionsort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let current = arr[i];
+    let j = i - 1;
+
+    while (j >= 0 && arr[j] > current) {
+      arr[j + 1] = arr[j];
+      j = j - 1;
+    }
+    arr[j + 1] = current;
+  }
+  console.log(arr);
+}
+
+//console.log(insertionsort(arr));
+insertionsort(arr);
