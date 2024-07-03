fe68fcd9acedbc5e2e50d155c87cf7a560a5fe52
Shobith S <shobithsugumar@gmail.com>
2024-07-03 22:32:41 +0530

    linedlist

--- hashtable.js
+++ hashtable.js
@@ -1,4 +1,4 @@
-class hashingg {
+/*class hashingg {
   constructor(size) {
     this.table = new Array(size);
   }
@@ -70,3 +70,76 @@ hashing.set("swetha", 30);
 
 console.log(hashing.remove("shobhit"));
 console.log(hashing.table);
+*/
+
+class hash {
+  constructor(size) {
+    this.table = new Array(size);
+  }
+
+  hashfun(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash += key.charCodeAt(i);
+    }
+    return hash % this.table.length;
+  }
+
+  set(key, value) {
+    let index = this.hashfun(key);
+    console.log(index);
+
+    if (this.table[index]) {
+      for (let i = 0; i < this.table[index].length; i++) {
+        console.log(this.table[index][i]["key"]);
+        if (this.table[index][i]["key"] === key) {
+          this.table[index][i]["value"] = value;
+          return;
+        }
+      }
+      this.table[index].push({ key, value });
+    } else {
+      this.table[index] = [];
+      this.table[index].push({ key, value });
+    }
+  }
+
+  get(key) {
+    let index = this.hashfun(key);
+
+    if (this.table[index]) {
+      for (let i = 0; i < this.table[index].length; i++) {
+        if (this.table[index][i]["key"] === key) {
+          console.log(this.table[index][i]);
+        }
+      }
+    }
+    return undefined;
+  }
+
+  remove(key) {
+    let index = this.hashfun(key);
+
+    if (this.table[index] && this.table[index].length) {
+      for (let i = 0; i < this.table[index].length; i++) {
+        if (this.table[index][i]["key"] === key) {
+          this.table[index].splice(i, 1);
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+  showall() {
+    console.log(this.table);
+  }
+}
+
+const hashing = new hash(10);
+
+hashing.set("apple", 10);
+hashing.set("appricot", 20);
+hashing.get("apple");
+hashing.get("appricot");
+hashing.remove("apple");
+hashing.showall();
--- linkedlist.js
+++ linkedlist.js
@@ -53,7 +53,7 @@ let link = new linkedlist(100);
 link.addnode(200);
 link.addnode(300);
 link.showall();
-*/
+
 
 class Node {
   constructor(data) {
@@ -124,12 +124,182 @@ class linkedlist {
   showall() {
     console.log("head", this.head);
     console.log("tail", this.tail);
+    console.log(this.length);
   }
 }
 
-let link = new linkedlist(100);
-link.push(200);
-link.push(300);
-link.push(400);
+let link = new linkedlist(50);
+link.unshift(100);
+link.unshift(25);
 
 link.showall();
+
+*/
+class Node {
+  constructor(data) {
+    this.value = data;
+    this.next = null;
+  }
+}
+
+class linkedlist {
+  constructor(data) {
+    this.head = new Node(data);
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  push(data) {
+    let newnode = new Node(data);
+
+    if (!this.head) {
+      this.head = newnode;
+      this.tail = this.head;
+    }
+    this.tail.next = newnode;
+    this.tail = newnode;
+    this.length++;
+  }
+
+  pop() {
+    let temp = this.head;
+    let prev = this.head;
+
+    if (!this.head) {
+      return undefined;
+    }
+
+    while (temp.next) {
+      prev = temp;
+      temp = prev.next;
+    }
+    this.tail = prev;
+    this.tail.next = null;
+    this.length--;
+
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+  }
+
+  unshift(data) {
+    let newnode = new Node(data);
+
+    if (!this.head) {
+      this.head = newnode;
+      this.tail = this.head;
+    }
+
+    newnode.next = this.head;
+    this.head = newnode;
+
+    this.length++;
+  }
+
+  //shift
+
+  shift() {
+    if (!this.head) {
+      return undefined;
+    }
+
+    //1. point the first node element
+
+    let temp = this.head;
+
+    //2. move the head to next node
+
+    this.head = this.head.next;
+
+    //3 remove the prev node
+
+    temp.next = null;
+    this.length--;
+
+    if (this.length === 0) {
+      this.tail = null;
+    }
+  }
+
+  showall() {
+    console.log("head", this.head);
+    console.log("tail", this.tail);
+    console.log(this.length);
+  }
+}
+let link = new linkedlist(100);
+
+link.push(200);
+link.push(300);
+link.shift();
+link.showall();
+
+//////////////////////////////////////////////////////////  USING SIMPLE FUNCTION
+
+function createNode(value) {
+  return {
+    value: value,
+    next: null,
+  };
+}
+
+function crtlinkedlist(value) {
+  if (value.length === 0) {
+    return undefined;
+  }
+  let head = createNode(value[0]);
+  let tail = head;
+
+  for (let i = 1; i < value.length; i++) {
+    tail.next = createNode(value[i]);
+    tail = tail.next;
+  }
+  return head;
+}
+
+let linkelist = crtlinkedlist([1]);
+
+function push(value, head) {
+  let newnode = createNode(value);
+
+  let current = head;
+
+  if (!head) {
+    head = newnode;
+    return head;
+  }
+
+  while (current.next !== null) {
+    current = current.next;
+  }
+
+  current.next = newnode;
+
+  return head;
+}
+
+function pop(head) {
+  if (!head) {
+    return undefined;
+  }
+
+  if (!head.next) {
+    return undefined;
+  }
+
+  let temp = head;
+  let prev = null;
+
+  while (temp.next) {
+    prev = temp;
+    temp = prev.next;
+  }
+  if (prev) {
+    prev.next = null;
+  }
+
+  return head;
+}
+
+console.log(pop(linkelist));
