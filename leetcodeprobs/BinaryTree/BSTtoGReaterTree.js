edc43af23135a6959b890ba83d2952b361ff9aaa
Shobith S <shobithsugumar@gmail.com>
2025-02-18 14:02:46 +0530

    leetcode

---
+++ leetcodeprobs/BinaryTree/BSTtoGReaterTree.js
@@ -0,0 +1,19 @@
+//problem : 538 convert binary search tree into greater tree
+//solution is to use dfs we go right first and then we go left
+
+var convertBST = function (root) {
+  let sum = 0;
+
+  if (!root) return null;
+
+  function dfs(root) {
+    if (!root) return;
+
+    dfs(root.right);
+    sum = sum + root.val;
+    root.val = sum;
+    dfs(root.left);
+    return root;
+  }
+  return dfs(root);
+};
