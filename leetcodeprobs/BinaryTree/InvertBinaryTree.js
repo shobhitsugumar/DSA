5ad55bca3076aa90b5fedb021cc533a096eaa311
Shobith S <shobithsugumar@gmail.com>
2025-04-25 04:58:10 +0530

    leetcode

---
+++ leetcodeprobs/BinaryTree/InvertBinaryTree.js
@@ -0,0 +1,22 @@
+//226. Invert Binary Tree
+
+/*https://leetcode.com/problems/invert-binary-tree/description/?envType=study-plan-v2&envId=top-interview-150
+ */
+
+//solution
+var invertTree = function (root) {
+  if (!root) return root;
+
+  function backtracking(root) {
+    if (!root) return;
+
+    let temp = root.left;
+    root.left = root.right;
+    root.right = temp;
+
+    backtracking(root.left);
+    backtracking(root.right);
+  }
+  backtracking(root);
+  return root;
+};
