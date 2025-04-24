bab01623dbb9eef64182c14fdbeaf252f1e4415b
Shobith S <shobithsugumar@gmail.com>
2025-04-25 04:40:05 +0530

    leetcode

--- leetcodeprobs/BinaryTree/SameTree.js
+++ leetcodeprobs/BinaryTree/SameTree.js
@@ -14,3 +14,21 @@ var isSameTree = function (node1, node2) {
     isSameTree(node1.right, node2.right)
   );
 };
+
+/*
+var isSameTree = function(node1, node2) {
+
+    
+  if(node1 === null&& node2===null){
+      return true
+  }
+  if(node1===null || node2===null){
+      return false
+  }
+
+  return (node1.val === node2.val) && isSameTree(node1.left,node2.left) && isSameTree(node1.right,node2.right)
+  
+
+  
+};
+*/
