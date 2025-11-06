3c52b4630a78a55697593acd41ae0d11ab9c696f
Shobith S <shobithsugumar@gmail.com>
2025-11-06 23:34:07 +0530

    leetcode

---
+++ 75days/hascycle.js
@@ -0,0 +1,17 @@
+var hasCycle = function (head) {
+  if (!head || head.next === null) return false;
+
+  let slow = head;
+  let fast = head;
+
+  while (fast !== null && fast.next !== null) {
+    fast = fast.next.next;
+    slow = slow.next;
+
+    if (fast === slow) {
+      return true;
+    }
+  }
+
+  return false;
+};
